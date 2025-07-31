543dafffa6713229c31b5aa4fc1f982dd439ac5d
Devesh951 <108172059+Devesh951@users.noreply.github.com>
2025-07-31 16:27:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ provider "aws" {
   access_key = "AKIAZDZTBMGDXJH54Q2C"
   secret_key = "o1rRRNgA8UmlB2ozwHBddRw0APorOaokc1Y1Fx/N"
 }
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DockerTerraformInstance"
+  }
+}
